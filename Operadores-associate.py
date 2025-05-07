27c78d5d6c02d12dd82eaf95cd5c40f6828181b3
Erick <erick.kine2012@hotmail.com>
2025-05-07 23:33:48 +0100

    Create Operadores-associate.py

---
+++ Operadores-associate.py
@@ -0,0 +1,10 @@
+cursos = "Cursos de Phyton"
+frutas = ["Laranjas", "Uva", "Limao"]
+saques = [1500, 100]
+
+# in = informativo de quando algo esta na sequencia
+print("Phyton" in cursos)
+print("coco" not in frutas)
+
+# not in = informativo de quando não esta na sequencia
+print (200 in saques)
