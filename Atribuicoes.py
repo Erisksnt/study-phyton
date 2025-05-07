78101707b07de12c69aa7fbc7e506c8681053407
Erick <erick.kine2012@hotmail.com>
2025-05-07 22:59:55 +0100

    Create Atribuicoes.py

---
+++ Atribuicoes.py
@@ -0,0 +1,27 @@
+#atribuição padrão
+saldo = 10
+print(saldo)
+
+#somando atribuição
+saldo += 200
+print(saldo)
+
+#subtraindo atribuição
+saldo -= 100
+print(saldo)
+
+#multiplicando atribuição
+saldo *= 10
+print(saldo)
+
+#dividindo por numero inteiro atribuição
+saldo //= 2
+print(saldo)
+
+#dividindo atribuição
+saldo /= 2
+print(saldo)
+
+#usando a potencia na atribuição
+saldo **=0
+print(saldo)
