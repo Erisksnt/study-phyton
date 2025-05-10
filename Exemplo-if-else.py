fd2a3670c3ffe990b8f87282be317acc4389b5cc
Erick <erick.kine2012@hotmail.com>
2025-05-10 03:12:21 +0100

    Update Exemplo-if-else.py

--- Exemplo-if-else.py
+++ Exemplo-if-else.py
@@ -13,3 +13,26 @@ elif idade >= maior_idade:
 #else = após if/elif se condição for falsa
 else:
     print ("menor de idade")
+#//////////////////////////////////////////////////////////////////////#
+
+conta_normal = False
+conta_universitaria = False
+
+saldo = 2000
+saque = 2450
+cheque_especial = 450
+
+if conta_normal:
+    if saldo >= saque:
+        print("saque realiado")
+    elif saque <= (saldo + cheque_especial):
+        print("saque feito usando cheque")
+    else:
+        print("saldo insuficiente mesmo com cheque")
+elif conta_universitaria:
+    if saldo >= saque:
+        print("saque realizado com sucesso")
+    else:
+        print("saldo insuficiente")
+else:
+    print("conta nao encontrada")
