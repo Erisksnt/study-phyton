f60a10919717bc47811633c880de18e81e8fdf38
Erick <erick.kine2012@hotmail.com>
2025-05-10 03:08:34 +0100

    Create string2.py

---
+++ string2.py
@@ -0,0 +1,22 @@
+###Exemplo de string2###
+
+nome = input("Informe your name: ")
+age = input("How old are u?: ")
+job = input("What your profission: ")
+language: input("What language do you use: ")
+
+#forma antiga (old style)
+print("nome: %s age: %s" % (nome, age))
+
+#forma normal
+print("nome: {} age: {}".format(nome, age))
+
+#outra forma, nessa forma e boa que posso repetir mais vezes se precisar
+print("nome: {1} age: {0}, {1} and {0}".format(nome, age))
+
+#outra forma com dicionario
+dados = {"nome": "guilherme", "age": 28}
+print("nome: {nome} age: {age}".format(**dados))
+
+#outra forma mais simples
+print(f"nome: {nome} age: {age}")
